ab0ed9af5163c4e7c3f5c3204e9835731f18a26c
lnascimento <almeida.luan099@gmail.com>
2018-12-10 17:30:26 -0200

    POO utilizando Flask

--- jogoteca.py
+++ jogoteca.py
@@ -2,9 +2,18 @@ from flask import Flask,render_template
 
 app = Flask(__name__)
 
+class Jogo:
+    def __init__(self,nome,categoria,console):
+        self.nome = nome
+        self.categoria = categoria
+        self.console = console
+
+
 @app.route("/inicio")
 def hello():
-    lista_jogos = ['Mortal Combat','Mario Cart','Top Gear']
+    jogo01 = Jogo('Super Mario','Acao','SNES')
+    jogo02 = Jogo('Pokemon Gold','RPG','GBA')
+    lista_jogos = [jogo01,jogo02]
     return render_template('lista.html',titulo='Lista de Jogos',jogos=lista_jogos)
 
-app.run(port=8080) #host='0.0.0.0',port=8080
+app.run(port=8081) #host='0.0.0.0',port=8080
