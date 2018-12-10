615622e77be0f4a4b85fde3d7ab25c5092f41921
lnascimento <almeida.luan099@gmail.com>
2018-12-10 18:12:48 -0200

    Criando jogo e salvando em lista

--- jogoteca.py
+++ jogoteca.py
@@ -1,6 +1,7 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 
 app = Flask(__name__)
+
 
 class Jogo:
     def __init__(self,nome,categoria,console):
@@ -8,12 +9,29 @@ class Jogo:
         self.categoria = categoria
         self.console = console
 
+#Lista global que ja vem com 2 jogos por default
+jogo01 = Jogo('Super Mario','Acao','SNES')
+jogo02 = Jogo('Pokemon Gold','RPG','GBA')
+lista = [jogo01,jogo02]
 
 @app.route("/inicio")
-def hello():
+def listaJogos():
     jogo01 = Jogo('Super Mario','Acao','SNES')
     jogo02 = Jogo('Pokemon Gold','RPG','GBA')
     lista_jogos = [jogo01,jogo02]
     return render_template('lista.html',titulo='Lista de Jogos',jogos=lista_jogos)
+
+@app.route("/novo")
+def novoJogo():
+    return render_template('novo.html',titulo="Novo Jogo")
+
+@app.route("/criar",methods=['POST',])
+def criarJogo():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+    jogo = Jogo(nome,categoria,console)
+    lista.append(jogo)
+    return render_template('lista.html',jogos=lista)
 
 app.run(port=8081) #host='0.0.0.0',port=8080
